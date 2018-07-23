467d6f4ae2091c8611dfab875e5ca5e86fe4da6a
bryce <brycelang@me.com>
2018-07-23 16:24:45 -0700

    JavaScript Framework
    Added a few variables and a method to initialize the game.

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//checks to make sure everything in the document has loaded properly.
+$(document).ready(function() { 
+    //variables to track wins, losses, and total score.
+    var win = 0;
+    var lose = 0;
+    var score = 0;
+
+    //initializes the game 
+function init(){
+    score = 0;
+
+}
+});
